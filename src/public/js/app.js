a9e57b38b23bb7aaa0019631d322a9326a5ec83c
XionWCFM <101111364+XionWCFM@users.noreply.github.com>
2022-12-19 04:50:24 +0900

    실시간 채팅 기능 및 사용자 특정 기능 추가 and add Comment file

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,25 +1,45 @@
 const messageList = document.querySelector('ul');
-const messageForm = document.querySelector('form');
+const nickForm = document.querySelector("#nick")
+const messageForm = document.querySelector("#message");
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+    const msg = { type, payload};
+    return JSON.stringify(msg)
+}
 
 socket.addEventListener("open" , () => {
     console.log("Connected to Server");
 })
 
 socket.addEventListener("message", (message) => {
-    console.log("just got this :" , message.data, "from the server");
+    const li = document.createElement("li")
+    li.innerText = message.data
+    console.log(message)
+    messageList.append(li)
 })
 
 socket.addEventListener("close" , () => {
     console.log("Disconnected from Server")
 })
 
+
+messageForm.addEventListener("submit" , handleSubmit)
+nickForm.addEventListener("submit" , handleNickSubmit)
+
+
 function handleSubmit(event) {
     event.preventDefault()
     const input = messageForm.querySelector("input")
-    socket.send(input.value);
+    socket.send(makeMessage("new_message" , input.value));
     input.value = "";
 
 }
 
-messageForm.addEventListener("submit" , handleSubmit)
+function handleNickSubmit(event) {
+    event.preventDefault();
+    const input = nickForm.querySelector("input")
+    socket.send(makeMessage("nickname" , input.value))
+    input.value = ""
+}
+
